2c7f53282e553b7971a16bc978aabdac2e1916b7
Lukáš Zaoral <x456487@fi.muni.cz>
2022-07-23 12:25:05 +0200

    args: simplify conversion error handling in JCKitConverter

--- src/main/java/jcprofiler/args/converters/JCKitConverter.java
+++ src/main/java/jcprofiler/args/converters/JCKitConverter.java
@@ -4,8 +4,6 @@ import com.beust.jcommander.ParameterException;
 import com.beust.jcommander.converters.BaseConverter;
 import pro.javacard.JavaCardSDK;
 
-import java.io.File;
-
 public class JCKitConverter extends BaseConverter<JavaCardSDK> {
     public JCKitConverter(final String optionName) {
         super(optionName);
@@ -13,15 +11,9 @@ public class JCKitConverter extends BaseConverter<JavaCardSDK> {
 
     @Override
     public JavaCardSDK convert(final String value) {
-        final File path = new File(value);
-        if (!path.isDirectory())
-            throw new ParameterException(String.format(
-                    "\"%s\": \"%s\" in not a valid path to a directory", getOptionName(), value));
-
         final JavaCardSDK jcSDK = JavaCardSDK.detectSDK(value);
         if (jcSDK == null)
-            throw new ParameterException(String.format(
-                    "\"%s\": \"%s\" in not a valid path to directory with JavaCard SDK", getOptionName(), value));
+            throw new ParameterException(getErrorString(value, "a valid path to directory with JavaCard SDK"));
 
         return jcSDK;
     }
