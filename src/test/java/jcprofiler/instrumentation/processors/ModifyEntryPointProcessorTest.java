a19ae3c7deed3c552984ec76d69a0006d93ea65d
Lukáš Zaoral <x456487@fi.muni.cz>
2022-08-09 22:13:21 +0200

    tests: add unit tests covering INS_PERF_SETSTOP switch cases insertion
    ... in ModifyEntryPointProcessor.

--- src/test/java/jcprofiler/instrumentation/processors/ModifyEntryPointProcessorTest.java
+++ src/test/java/jcprofiler/instrumentation/processors/ModifyEntryPointProcessorTest.java
@@ -119,6 +119,159 @@ class ModifyEntryPointProcessorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void pmWrongTrapType() {
+        // Using already instrumented file for input is NOT a bug!
+        final CtClass<?> input = parseClass("ModifyEntryPointProcessorTestExpected.java");
+        final CtClass<?> pmClass = input.getPackage().getType("PM");
+        final CtField<?> perfStopField = pmClass.getField("m_perfStop");
+
+        // change type of PM.m_perfStop from short to byte
+        perfStopField.setType(input.getFactory().createCtTypeReference(Byte.TYPE));
+
+        Exception e = assertThrows(
+                RuntimeException.class,
+                () -> assertThat(input).withProcessor(new ModifyEntryPointProcessor(new Args())).isEqualTo(input));
+
+        String expected = "PM.m_perfStop has type byte! Expected: short";
+        String actual = e.getMessage();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void missingProcessMethod() {
+        // Using already instrumented file for input is NOT a bug!
+        final CtClass<?> input = parseClass("ModifyEntryPointProcessorTestExpected.java");
+
+        // delete the process method
+        input.getMethodsByName("process").get(0).delete();
+
+        Exception e = assertThrows(
+                RuntimeException.class,
+                () -> assertThat(input).withProcessor(new ModifyEntryPointProcessor(new Args())).isEqualTo(input));
+
+        String expected = "Class Example inherits from javacard.framework.Applet but does not implement the 'process' method!";
+        String actual = e.getMessage();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void declaredProcessMethod() {
+        // Using already instrumented file for input is NOT a bug!
+        final CtClass<?> input = parseClass("ModifyEntryPointProcessorTestExpected.java");
+
+        // delete the process method body
+        input.getMethodsByName("process").get(0).getBody().delete();
+
+        Exception e = assertThrows(
+                RuntimeException.class,
+                () -> assertThat(input).withProcessor(new ModifyEntryPointProcessor(new Args())).isEqualTo(input));
+
+        String expected = "Class Example inherits from javacard.framework.Applet but does not implement the 'process' method!";
+        String actual = e.getMessage();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void noSwitchesInProcess() {
+        // Using already instrumented file for input is NOT a bug!
+        final CtClass<?> input = parseClass("ModifyEntryPointProcessorTestExpected.java");
+        final CtSwitch<Byte> ctSwitch = input.filterChildren(CtSwitch.class::isInstance).first();
+
+        // remove the switch in process method
+        ctSwitch.delete();
+
+        Exception e = assertThrows(
+                RuntimeException.class,
+                () -> assertThat(input).withProcessor(new ModifyEntryPointProcessor(new Args())).isEqualTo(input));
+
+        String expected = String.format(
+                "No switch statement with byte selector found in Example.process method!%nPlease adapt your " +
+                "applet so that it uses a single switch statement to determine the instruction to execute.");
+        String actual = e.getMessage();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void moreSwitchesInProcess() {
+        // Using already instrumented file for input is NOT a bug!
+        final CtClass<?> input = parseClass("ModifyEntryPointProcessorTestExpected.java");
+        final CtStatementList processMethodBody = input.getMethodsByName("process").get(0).getBody();
+        final CtSwitch<?> originalSwitch = input.filterChildren(CtSwitch.class::isInstance).first();
+
+        // add a second byte switch to the process method
+        final CtSwitch<Byte> newSwitch = input.getFactory().createSwitch();
+        newSwitch.setSelector(input.getFactory().createLiteral((byte) 1));
+        processMethodBody.addStatement(newSwitch);
+
+        Exception e = assertThrows(
+                RuntimeException.class,
+                () -> assertThat(input).withProcessor(new ModifyEntryPointProcessor(new Args())).isEqualTo(input));
+
+        String expected = String.format(
+                "More switch statements with byte selector found in Example.process method!%nPlease adapt your " +
+                "applet so that it uses a single switch statement to determine the instruction to execute.%n" +
+                "Found:%n%s%n%s%n", originalSwitch.prettyprint(), newSwitch.prettyprint());
+        String actual = e.getMessage();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void alreadyUsedSwitchCase() {
+        // Using already instrumented file for input is NOT a bug!
+        final CtClass<?> input = parseClass("ModifyEntryPointProcessorTestExpected.java");
+        final CtSwitch<Byte> ctSwitch = input.filterChildren(CtSwitch.class::isInstance).first();
+        final CtCase<? super Byte> ctCase = ctSwitch.getCases().stream().filter(c -> {
+            final CtLiteral<? extends Number> lit = c.getCaseExpression().partiallyEvaluate();
+            return lit.getValue().byteValue() == JCProfilerUtil.INS_PERF_SETSTOP;
+        }).findAny().get();
+
+        // change the case to the same value but with a different source
+        ctCase.setCaseExpression(input.getFactory().createLiteral(JCProfilerUtil.INS_PERF_SETSTOP));
+
+        Exception e = assertThrows(
+                RuntimeException.class,
+                () -> assertThat(input).withProcessor(new ModifyEntryPointProcessor(new Args())).isEqualTo(input));
+
+        String expected = String.format(
+                "The switch in process method already contains a case for 0x%02X distinct from the " +
+                   "INS_PERF_SETSTOP field:%n%s", JCProfilerUtil.INS_PERF_SETSTOP, JCProfilerUtil.INS_PERF_SETSTOP);
+        String actual = e.getMessage();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void wrongSwitchCaseBody() {
+        // Using already instrumented file for input is NOT a bug!
+        final CtClass<?> input = parseClass("ModifyEntryPointProcessorTestExpected.java");
+        final CtSwitch<Byte> ctSwitch = input.filterChildren(CtSwitch.class::isInstance).first();
+        final CtCase<? super Byte> ctCase = ctSwitch.getCases().stream().filter(c -> {
+            final CtLiteral<? extends Number> lit = c.getCaseExpression().partiallyEvaluate();
+            return lit.getValue().byteValue() == JCProfilerUtil.INS_PERF_SETSTOP;
+        }).findAny().get();
+
+        // change the case body
+        final CtCase<? super Byte> ctCaseBackup = ctCase.clone();
+        ctCase.getStatement(0).delete();
+
+        Exception e = assertThrows(
+                RuntimeException.class,
+                () -> assertThat(input).withProcessor(new ModifyEntryPointProcessor(new Args())).isEqualTo(input));
+
+        String expected = String.format(
+                "The body of the INS_PERF_SETSTOP switch case has unexpected contents:%n%s%nExpected:%n%s%n",
+                ctCase.prettyprint(), ctCaseBackup.prettyprint());
+        String actual = e.getMessage();
+
+        assertEquals(expected, actual);
+    }
+
     public CtClass<?> parseClass(final String fileName) {
         final Launcher spoon = new Launcher();
 
