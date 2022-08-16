e4e9b3003698edc5c88e7fd404d0326456382df3
Lukáš Zaoral <x456487@fi.muni.cz>
2022-08-16 16:24:09 +0200

    main: add basic logging

--- src/main/java/jcprofiler/Main.java
+++ src/main/java/jcprofiler/Main.java
@@ -3,19 +3,49 @@ package jcprofiler;
 import com.beust.jcommander.JCommander;
 import jcprofiler.args.Args;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(final String[] argv) {
+        Configurator.setRootLevel(Level.INFO);
+        log.info("Welcome to JCProfiler Next!");
+
         Args args = new Args();
         JCommander jc = JCommander.newBuilder().addObject(args).build();
 
         jc.setProgramName("JCProfiler");
-        jc.parse(argv);
+        try {
+            jc.parse(argv);
+        } catch (Exception e) {
+            log.error("Argument parsing failed!", e);
+            System.exit(1);
+        }
 
         if (args.help) {
             jc.usage();
             return;
         }
 
-        JCProfiler.run(args);
+        log.info("Command-line arguments parsed successfully.");
+        if (args.debug) {
+            Configurator.setRootLevel(Level.DEBUG);
+            log.info("LogLevel set to DEBUG.");
+        }
+
+        log.info("Found JavaCard SDK {} ({})", args.jcSDK.getRelease(), args.jcSDK.getRoot().getAbsolutePath());
+        log.info("Working directory: {}", args.workDir.toAbsolutePath());
+
+        try {
+            JCProfiler.run(args);
+            log.info("Success!");
+        } catch (Exception e) {
+            log.error("Caught exception!", e);
+            System.exit(1);
+        }
     }
 }
