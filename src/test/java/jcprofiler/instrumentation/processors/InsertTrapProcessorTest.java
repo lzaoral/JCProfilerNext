555c4741eed64dd47a301fa741305447d259d68b
Lukáš Zaoral <x456487@fi.muni.cz>
2022-08-19 14:38:31 +0200

    tests: simplify test method and trap detection in InsertTrapProcessorTest

--- src/test/java/jcprofiler/instrumentation/processors/InsertTrapProcessorTest.java
+++ src/test/java/jcprofiler/instrumentation/processors/InsertTrapProcessorTest.java
@@ -1,31 +1,31 @@
 package jcprofiler.instrumentation.processors;
 
 import jcprofiler.args.Args;
+
+import jcprofiler.util.JCProfilerUtil;
 import org.junit.jupiter.api.Test;
+
 import spoon.Launcher;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
 import spoon.support.compiler.VirtualFile;
 import spoon.testing.AbstractCtElementAssert;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 import static spoon.testing.Assert.assertThat;
 
 class InsertTrapProcessorTest {
-    // TODO: instrument everything when method is null?
-    final static List<String> methods = Arrays.asList(
-            "foo",
-            "terminator1", "terminator2", "terminator3", "terminator4", "terminator5", "terminator6", "terminator7",
-            "terminator8");
-
     @Test
     public void process() {
-        final CtClass<?> input = parseClass("InsertTrapProcessorTestInput.java");
-        final CtClass<?> expected = parseClass("InsertTrapProcessorTestExpected.java");
+        final CtClass<?> input = parseInputClass("InsertTrapProcessorTestInput.java");
+        final CtClass<?> expected = parseExpectedClass(input, "InsertTrapProcessorTestExpected.java");
 
-        AbstractCtElementAssert<?> assertThat = assertThat(input);
+        final List<String> methods = input.filterChildren(CtMethod.class::isInstance)
+                                          .map((CtMethod<?> m) -> m.getSimpleName()).list();
+
+        AbstractCtElementAssert <?> assertThat = assertThat(input);
         for (String method : methods) {
             final Args args = new Args();
             args.method = method;
@@ -34,33 +34,42 @@ class InsertTrapProcessorTest {
         assertThat.isEqualTo(expected);
     }
 
-    public CtClass<?> parseClass(final String fileName) {
+    public CtClass<?> parseInputClass(final String fileName) {
         final Launcher spoon = new Launcher();
 
         // add PM stub
-        spoon.addInputResource(new VirtualFile(
-                "public class PM { public static void check(short s) {} }", "PM.java"));
-
-        // SPOON must know the types of PMC fields used in SimpleClass
-        if (fileName.equals("InsertTrapProcessorTestExpected.java")) {
-            StringBuilder sb = new StringBuilder("public class PMC { ");
-
-            for (String method : methods) {
-                for (int i = 1; i <= 100; i++)
-                    sb.append("public static short TRAP_SimpleClass_").append(method).append("_argb_int_arge_")
-                            .append(i).append(" = 0;");
-            }
-
-            sb.append("}");
-            spoon.addInputResource(new VirtualFile(sb.toString(), "PMC.java"));
-        } else {
-            spoon.addInputResource(new VirtualFile("public class PMC {}", "PMC.java"));
-        }
+        spoon.addInputResource(new VirtualFile("public class PM { public static void check(short s) {} }"));
+        spoon.addInputResource(new VirtualFile("public class PMC {}"));
 
         // add the input
         spoon.addInputResource(Objects.requireNonNull(getClass().getResource(fileName)).getPath());
         spoon.buildModel();
 
-        return spoon.getModel().getElements((CtClass<?> cls) -> cls.getSimpleName().equals("SimpleClass")).get(0);
+        return spoon.getModel().filterChildren((CtClass<?> cls) -> cls.getSimpleName().equals("SimpleClass")).first();
+    }
+
+    public CtClass<?> parseExpectedClass(final CtClass<?> input, final String fileName) {
+        final Launcher spoon = new Launcher();
+
+        // add PM stub
+        spoon.addInputResource(new VirtualFile("public class PM { public static void check(short s) {} }"));
+
+        // SPOON must know the types of PMC fields used in SimpleClass
+        final List<CtMethod<?>> methods = input.getElements(CtMethod.class::isInstance);
+        StringBuilder sb = new StringBuilder("public class PMC {");
+        for (final CtMethod<?> method : methods) {
+            for (int i = 1; i <= 100; i++)
+                sb.append("public static short ").append(JCProfilerUtil.getTrapNamePrefix(method)).append("_").append(i)
+                        .append(" = 0;");
+        }
+        sb.append("}");
+
+        spoon.addInputResource(new VirtualFile(sb.toString()));
+
+        // add the input
+        spoon.addInputResource(Objects.requireNonNull(getClass().getResource(fileName)).getPath());
+        spoon.buildModel();
+
+        return spoon.getModel().filterChildren((CtClass<?> cls) -> cls.getSimpleName().equals("SimpleClass")).first();
     }
 }
