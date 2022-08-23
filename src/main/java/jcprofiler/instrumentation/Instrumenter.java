lzaoral/JCProfilerNext

commit da9ffb9d8a01a34c9ae5bc92d38d8a90e01ab290

jcprofiler: support profiling of overloaded methods
... or of methods in nested classes.

src/main/java/jcprofiler/instrumentation/processors/InsertTrapProcessor.java
@@ -1,6 +1,8 @@
 package jcprofiler.instrumentation.processors;
 
 import jcprofiler.args.Args;
+import jcprofiler.util.JCProfilerUtil;
+
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.*;
 import spoon.reflect.declaration.*;
@@ -35,9 +37,7 @@ public class InsertTrapProcessor extends AbstractProcessor<CtMethod<?>> {
             PMC = method.getDeclaringType().getPackage().getType("PMC");
 
         trapCount = 1;
-
-        // TODO: what about method overloading?
-        trapNamePrefix = getTrapNamePrefix(method);
+        trapNamePrefix = JCProfilerUtil.getTrapNamePrefix(method);
 
         if (args.maxTraps * methodCount >= 0xffff)
             throw new RuntimeException(String.format("Class has more than %d (%d)",
@@ -52,22 +52,6 @@ public class InsertTrapProcessor extends AbstractProcessor<CtMethod<?>> {
                                 method.getSignature())));
 
         processBody(method.getBody());
-    }
-
-    private String getTrapNamePrefix(final CtMethod<?> method) {
-        // simple name should be enough as all classes should be in the same package
-        // TODO: what about nested classes?
-        final String prefix = String.format(
-                "TRAP_%s_%s", method.getDeclaringType().getSimpleName(), method.getSignature());
-
-        // list may not be exhaustive
-        return prefix.replace('.', '_') // used in qualified types
-                .replace(",", "__") // args delimiter
-                .replace("(", "_argb_")
-                .replace(")", "_arge")
-                .replace("<", "_genb_")
-                .replace(">", "_gene_")
-                .replace("[]", "arr"); // used in arrays
     }
 
     private void processBody(final CtStatementList block) {

src/main/java/jcprofiler/profiling/Profiler.java
@@ -12,10 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import spoon.SpoonAPI;
-import spoon.reflect.CtModel;
 import spoon.reflect.code.CtLiteral;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtField;
+import spoon.reflect.declaration.CtMethod;
 
 import javax.smartcardio.CardException;
 import javax.smartcardio.CommandAPDU;
@@ -35,7 +35,7 @@ public class Profiler {
     private static final short PERF_START = 0x0001;
     private final Args args;
     private final CardManager cardManager;
-    private final CtModel model;
+    private final SpoonAPI spoon;
 
     // use LinkedHashX to preserve insertion order
     private final Map<Short, String> trapNameMap = new LinkedHashMap<>();
@@ -48,7 +48,7 @@ public class Profiler {
     public Profiler(final Args args, final CardManager cardManager, final SpoonAPI spoon) {
         this.args = args;
         this.cardManager = cardManager;
-        this.model = spoon.getModel();
+        this.spoon = spoon;
 
         buildPerfMapping();
     }
@@ -58,16 +58,15 @@ public class Profiler {
     }
 
     private void buildPerfMapping() {
-        // TODO: use a more precise method name
-        log.info("Looking for traps in {}.", args.method);
+        log.info("Looking for traps in the {} method.", args.method);
+        final CtMethod<?> profiledMethod = JCProfilerUtil.getProfiledMethod(spoon, args.method);
+        final String trapNamePrefix = JCProfilerUtil.getTrapNamePrefix(profiledMethod) + "_";
 
         // TODO: what about more classes with such name?
         // idea: make PM and PMC both inherit from our special and empty abstract class
-        final CtClass<?> pmc = model.filterChildren((CtClass<?> c) -> c.getSimpleName().equals("PMC")).first();
+        final CtClass<?> pmc = spoon.getModel().filterChildren((CtClass<?> c) -> c.getSimpleName().equals("PMC")).first();
 
-        // FIXME: this may fail, may get more methods, due to dependence on mangling
-        final String regex = String.format(".*_%s_argb_.*_arge_[^_]*", args.method);
-        final List<CtField<Short>> traps = pmc.getElements((CtField<Short> f) -> f.getSimpleName().matches(regex));
+        final List<CtField<Short>> traps = pmc.getElements((CtField<Short> f) -> f.getSimpleName().startsWith(trapNamePrefix));
         if (traps.isEmpty())
             throw new RuntimeException(String.format(
                     "Extraction of traps from PM for %s failed!", args.method));

src/main/java/jcprofiler/util/JCProfilerUtil.java
@@ -127,6 +127,21 @@ public class JCProfilerUtil {
         return method;
     }
 
+    // trap mangling
+    static public String getTrapNamePrefix(final CtMethod<?> method) {
+        final String prefix = "TRAP_" + method.getDeclaringType().getQualifiedName() + "_" + method.getSignature();
+
+        // list may not be exhaustive
+        return prefix.replace('.', '_') // used in qualified types
+                .replace("$", "_dol_") // nested class
+                .replace(",", "__") // args delimiter
+                .replace("(", "_argb_")
+                .replace(")", "_arge")
+                .replace("<", "_genb_")
+                .replace(">", "_gene_")
+                .replace("[]", "arr"); // used in arrays
+    }
+
     public static boolean isClsEntryPoint(final CtClass<?> cls) {
         final CtTypeReference<?> parent = cls.getSuperclass();
         return parent != null && parent.getQualifiedName().equals("javacard.framework.Applet");

commit 2806a848291723219916ffcc00b0da6ebd983376

instrumentation: fix broken method filtering in InsertTrapProcessor
Fixes: da9ffb9

src/main/java/jcprofiler/instrumentation/Instrumenter.java
@@ -12,6 +12,7 @@ import spoon.Launcher;
 import spoon.OutputType;
 import spoon.SpoonAPI;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtPackage;
 import spoon.support.compiler.VirtualFile;
 
@@ -76,8 +77,9 @@ public class Instrumenter {
         // validate args.entryPoint
         JCProfilerUtil.getEntryPoint(spoon, args.entryPoint);
 
-        // validate args.method
-        JCProfilerUtil.getProfiledMethod(spoon, args.method);
+        // validate and select args.method
+        final CtMethod<?> method = JCProfilerUtil.getProfiledMethod(spoon, args.method);
+        args.method = method.getDeclaringType().getQualifiedName() + "." + method.getSignature();
     }
 
     public static void setupSpoon(final SpoonAPI spoon, final Args args) {

src/main/java/jcprofiler/instrumentation/processors/InsertTrapProcessor.java
@@ -30,9 +30,8 @@ public class InsertTrapProcessor extends AbstractProcessor<CtMethod<?>> {
 
     @Override
     public boolean isToBeProcessed(final CtMethod<?> method) {
-        // TODO: comment
-        return method.getBody() != null && !method.getDeclaringType().getSimpleName().matches("PMC?")
-                && method.getSimpleName().equals(args.method);
+        final String actualMethodSignature = method.getDeclaringType().getQualifiedName() + "." + method.getSignature();
+        return actualMethodSignature.equals(args.method);
     }
 
     @Override

src/test/java/jcprofiler/instrumentation/processors/InsertTrapProcessorTest.java
@@ -23,7 +23,7 @@ class InsertTrapProcessorTest {
         final CtClass<?> expected = parseExpectedClass(input, "InsertTrapProcessorTestExpected.java");
 
         final List<String> methods = input.filterChildren(CtMethod.class::isInstance)
-                                          .map((CtMethod<?> m) -> m.getSimpleName()).list();
+                .map((CtMethod<?> m) -> m.getDeclaringType().getQualifiedName() + "." + m.getSignature()).list();
 
         AbstractCtElementAssert <?> assertThat = assertThat(input);
         for (String method : methods) {