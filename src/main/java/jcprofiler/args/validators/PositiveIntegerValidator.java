82d9e8b76e4b268f656cff1f4bd4c509a0a405af
Lukáš Zaoral <x456487@fi.muni.cz>
2022-10-30 11:05:55 +0100

    args: be more verbose about parsing errors in PositiveIntegerValidator

--- src/main/java/jcprofiler/args/validators/PositiveIntegerValidator.java
+++ src/main/java/jcprofiler/args/validators/PositiveIntegerValidator.java
@@ -5,10 +5,15 @@ import com.beust.jcommander.ParameterException;
 
 public class PositiveIntegerValidator implements IParameterValidator {
     @Override
-    public void validate(String name, String value) throws ParameterException {
-        int n = Integer.parseInt(value);
-        if (n < 1)
+    public void validate(final String name, final String value) throws ParameterException {
+        try {
+            int n = Integer.parseInt(value);
+            if (n < 1)
+                throw new ParameterException(String.format(
+                        "\"%s\": \"%s\" is not a positive integer", name, value));
+        } catch (NumberFormatException e) {
             throw new ParameterException(String.format(
-                    "\"%s\": \"%s\" is not a positive integer", name, value));
+                    "\"%s\": \"%s\" is not a positive integer", name, value), e);
+        }
     }
 }
