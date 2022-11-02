860732c13f0221d94d106233e6da527aaad96ad9
Lukáš Zaoral <x456487@fi.muni.cz>
2022-11-02 15:36:33 +0100

    tests: add gather-stats.sh script
    This script collect API usage statistics from all repositories defined
    in test-data.json.

---
+++ tests/gather-stats.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -eu
+
+echo "Gathering API usage statistics"
+rm -r stats_out stats.csv || true
+./run.py --stats --output-dir stats_out
+
+set -- stats_out/*
+for f in "$@"; do
+    {
+        APPLET_DIR="$(basename "$f" | sed -e 's/_stats_.*$//')"
+        REPO_URL="$(git -C "$APPLET_DIR" remote get-url origin)"
+
+        # print header
+        echo "# Applet: $APPLET_DIR"
+        echo "# Repo: $REPO_URL"
+        echo
+
+        # append stats
+        cat "$f/APIstatistics.csv"
+        printf "\n\n"
+    } >> stats.txt
+done
+
+echo "Analysed $# projects."
+INCOMPLETE_COUNT="$(grep -c 'CSV generated in permissive mode!' stats.txt)"
+echo "$INCOMPLETE_COUNT did not have all dependencies on class path!"
+
+echo "stats.txt generated successfully."
