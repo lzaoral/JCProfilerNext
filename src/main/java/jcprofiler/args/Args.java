lzaoral/JCProfilerNext

commit 102c664c8bbff610fe8b815603abe28a297c05b6

args: merge --input-dir and --output-dir into a single option
... so that it is less confusing to see what is the actual input or output
when the --start-from option is introduced.

src/main/java/jcprofiler/args/Args.java
@@ -3,15 +3,15 @@ package jcprofiler.args;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.validators.PositiveInteger;
 import jcprofiler.args.converters.StageConverter;
-import jcprofiler.util.Stage;
+import jcprofiler.args.converters.DirectoryConverter;
 import jcprofiler.args.converters.ByteConverter;
 import jcprofiler.args.converters.JCKitConverter;
 import jcprofiler.args.validators.InputPathValidator;
-import jcprofiler.args.validators.OutputPathValidator;
 import jcprofiler.args.validators.RegexValidator;
+import jcprofiler.util.Stage;
 import pro.javacard.JavaCardSDK;
 
-import java.util.List;
+import java.nio.file.Path;
 
 public class Args {
     @Parameter(names = {"-h", "--help"},
@@ -19,18 +19,11 @@ public class Args {
                help = true)
     public boolean help = false;
 
-    // TODO: add some filter for input files?
-    @Parameter(names = {"-i", "--input-file", "--input-dir"},
+    @Parameter(names = {"-w", "--work-dir"},
                description = "Input files or directories (can be specified multiple times)",
                required = true,
-               validateWith = InputPathValidator.class)
-    public List<String> inputs;
-
-    @Parameter(names = {"-o", "--output-dir"},
-               description = "Output directory",
-               required = true,
-               validateWith = OutputPathValidator.class)
-    public String outputDir;
+               converter = DirectoryPathConverter.class)
+    public Path workDir;
 
     @Parameter(names = {"--stop-after"},
                description = "Stop after executing the given stage",

src/main/java/jcprofiler/JCProfiler.java
@@ -28,7 +28,7 @@ public class JCProfiler {
         // check that the generated sources are compilable by rebuilding the model after instrumentation
         final SpoonAPI spoon = new Launcher();
         Instrumenter.setupSpoon(spoon, args);
-        spoon.addInputResource(args.outputDir);
+        spoon.addInputResource(JCProfilerUtil.getInstrOutputDirectory(args.workDir).toString());
         spoon.buildModel();
 
         if (args.stopAfter == Stage.instrumentation)

src/main/java/jcprofiler/args/converters/DirectoryPathConverter.java
@@ -1,0 +1,21 @@
+package jcprofiler.args.converters;
+
+import com.beust.jcommander.ParameterException;
+import com.beust.jcommander.converters.BaseConverter;
+
+import java.io.File;
+import java.nio.file.Path;
+
+public class DirectoryPathConverter extends BaseConverter<Path> {
+    public DirectoryPathConverter(String optionName) {
+        super(optionName);
+    }
+
+    @Override
+    public Path convert(String value) {
+        final File input = new File(value);
+        if (!input.isDirectory())
+            throw new ParameterException(getErrorString(value, "a path to existing directory"));
+        return input.toPath();
+    }
+}

src/main/java/jcprofiler/args/validators/OutputPathValidator.java
@@ -1,24 +1,0 @@
-package jcprofiler.args.validators;
-
-import com.beust.jcommander.IParameterValidator;
-import com.beust.jcommander.ParameterException;
-
-import java.io.File;
-import java.util.Objects;
-
-public class OutputPathValidator implements IParameterValidator {
-    @Override
-    public void validate(final String name, final String value) throws ParameterException {
-        final File path = new File(value);
-        if (path.exists() && !path.isDirectory())
-            throw new ParameterException(String.format(
-                    "\"%s\": \"%s\" is not a path to directory", name, value));
-
-        if (path.isDirectory() && Objects.requireNonNull(path.list()).length > 1)
-            System.err.printf("WARNING: Path %s is non-empty!%n", path);
-
-        if (!path.exists() && !path.mkdirs())
-            throw new ParameterException(String.format(
-                    "Directories in path %s were not created successfully!", value));
-    }
-}

src/main/java/jcprofiler/compilation/Compiler.java
@@ -1,6 +1,7 @@
 package jcprofiler.compilation;
 
 import jcprofiler.args.Args;
+import jcprofiler.util.JCProfilerUtil;
 import org.apache.tools.ant.DefaultLogger;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Target;
@@ -10,7 +11,9 @@ import pro.javacard.ant.JavaCard.JCApplet;
 import pro.javacard.ant.JavaCard.JCCap;
 import spoon.reflect.declaration.CtClass;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Compiler {
@@ -19,10 +22,18 @@ public class Compiler {
 
     // Inspired by https://stackoverflow.com/questions/6440295/is-it-possible-to-call-ant-or-nsis-scripts-from-java-code/6440342#6440342
     public static void compile(final Args args, final CtClass<?> entryPoint) {
+        // create the output directory if it does not exist
+        final Path appletDir = JCProfilerUtil.getAppletOutputDirectory(args.workDir).toAbsolutePath();
+        try {
+            Files.createDirectories(appletDir);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         final Project p = new Project();
         p.init();
         p.setName("JavaCard");
-        p.setBaseDir(new File(args.outputDir));
+        p.setBaseDir(appletDir.toFile());
 
         /*
          TODO: make it possible to select the JDK that is used to compile the project if possible
@@ -55,8 +66,8 @@ public class Compiler {
         final JCCap cap = jc.createCap();
         cap.setTaskName("JavaCard");
         cap.setProject(p);
-        cap.setSources(Paths.get(args.outputDir, "sources").toFile().getAbsolutePath());
-        cap.setExport("bin");
+        cap.setSources(Paths.get("..", JCProfilerUtil.INSTR_OUT_DIRNAME).toString());
+        cap.setExport(".");
 
         // TODO: check if this ok
         cap.setDebug(false);
@@ -64,7 +75,7 @@ public class Compiler {
 
         cap.setAID("1234567890");
         cap.setPackage(entryPoint.getPackage().getQualifiedName());
-        cap.setOutput(String.format("%s.cap", entryPoint.getSimpleName()));
+        cap.setOutput(entryPoint.getSimpleName() + ".cap");
 
         final JCApplet app = cap.createApplet();
         app.setClass(entryPoint.getQualifiedName());

src/main/java/jcprofiler/installation/Installer.java
@@ -9,6 +9,7 @@ import cz.muni.fi.crocs.rcard.client.RunConfig;
 import cz.muni.fi.crocs.rcard.client.Util;
 import javacard.framework.Applet;
 import jcprofiler.args.Args;
+import jcprofiler.util.JCProfilerUtil;
 import pro.javacard.gp.GPTool;
 import pro.javacard.gp.ISO7816;
 import spoon.reflect.declaration.CtClass;
@@ -18,7 +19,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -33,7 +33,8 @@ public class Installer {
     }
 
     private static CardManager configureSimulator(final Args args, final CtClass<?> entryPoint) {
-        final Path jarPath = Paths.get(args.outputDir, "bin", entryPoint.getPackage().getSimpleName() + ".jar");
+        final Path jarPath = JCProfilerUtil.getAppletOutputDirectory(args.workDir)
+                .resolve(entryPoint.getPackage().getSimpleName() + ".jar");
         final CardManager cardManager = new CardManager(false, Util.hexStringToByteArray("123456789001"));
 
         try {
@@ -57,7 +58,8 @@ public class Installer {
     }
 
     private static CardManager installOnCard(final Args args, final CtClass<?> entryPoint) {
-        final Path capPath = Paths.get(args.outputDir, entryPoint.getSimpleName() + ".cap");
+        final Path capPath = JCProfilerUtil.getAppletOutputDirectory(args.workDir)
+                .resolve(entryPoint.getSimpleName() + ".cap");
         final CardManager cardManager = new CardManager(false, Util.hexStringToByteArray("123456789001"));
         // we do it manually later after we have successfully installed the applet
         cardManager.setDoSelect(false);

src/main/java/jcprofiler/instrumentation/Instrumenter.java
@@ -13,7 +13,6 @@ import spoon.reflect.declaration.CtPackage;
 import spoon.support.compiler.VirtualFile;
 
 import java.io.*;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -41,8 +40,9 @@ public class Instrumenter {
         spoon.addProcessor(new InsertTrapProcessor(args));
         spoon.process();
 
-        // compile the result???
+        // save the result
         spoon.getEnvironment().setOutputType(OutputType.CLASSES);
+        spoon.setSourceOutputDirectory(JCProfilerUtil.getInstrOutputDirectory(args.workDir).toFile());
         spoon.prettyprint();
 
         // TODO sanity check that all PMC members are unique
@@ -50,7 +50,7 @@ public class Instrumenter {
 
     private void buildModel(final Launcher spoon) {
         setupSpoon(spoon, args);
-        args.inputs.forEach(spoon::addInputResource);
+        spoon.addInputResource(args.workDir.toString());
         spoon.buildModel();
     }
 
@@ -73,7 +73,6 @@ public class Instrumenter {
         // TODO: uncommenting this might lead to SPOON crashes!
         // spoon.getEnvironment().setPrettyPrinterCreator(() -> new SniperJavaPrettyPrinter(spoon.getEnvironment()));
 
-        spoon.setSourceOutputDirectory(Paths.get(args.outputDir, "sources").toFile());
         spoon.getEnvironment().setNoClasspath(false);
         spoon.getEnvironment().setAutoImports(true);
         spoon.getEnvironment().setCopyResources(false);

src/main/java/jcprofiler/util/JCProfilerUtil.java
@@ -4,11 +4,16 @@ import spoon.SpoonAPI;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.reference.CtTypeReference;
 
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
 
 public class JCProfilerUtil {
     public static final byte INS_PERF_SETSTOP = (byte) 0xf5;
+
+    public static final String APPLET_OUT_DIRNAME = "applet";
+    public static final String INSTR_OUT_DIRNAME  = "sources_instr";
+    public static final String PERF_OUT_DIRNAME   = "sources_perf";
 
     // static class!
     private JCProfilerUtil() {}
@@ -44,5 +49,17 @@ public class JCProfilerUtil {
     public static boolean isClsEntryPoint(final CtClass<?> cls) {
         final CtTypeReference<?> parent = cls.getSuperclass();
         return parent != null && parent.getQualifiedName().equals("javacard.framework.Applet");
+    }
+
+    public static Path getInstrOutputDirectory(final Path workDirPath) {
+        return workDirPath.resolve(INSTR_OUT_DIRNAME);
+    }
+
+    public static Path getPerfOutputDirectory(final Path workDirPath) {
+        return workDirPath.resolve(PERF_OUT_DIRNAME);
+    }
+
+    public static Path getAppletOutputDirectory(final Path workDirPath) {
+        return workDirPath.resolve(APPLET_OUT_DIRNAME);
     }
 }

commit b279aaea8fd17ad0b9e0fbe5c7099346bdef6117

args: fix description of the --work-dir option
Unfortunately, I forgot to update it when I merged the --input-dir
and --output-dir options into a single one.

Fixes: 102c664c

src/main/java/jcprofiler/args/Args.java
@@ -23,7 +23,7 @@ public class Args {
     public boolean debug = false;
 
     @Parameter(names = {"-w", "--work-dir"},
-               description = "Input files or directories (can be specified multiple times)",
+               description = "Path to the working directory",
                required = true,
                converter = DirectoryPathConverter.class)
     public Path workDir;