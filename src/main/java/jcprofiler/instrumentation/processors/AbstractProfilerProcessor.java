126c6593a68bd51d9d70a459316f5cc15a9c29ae
Lukáš Zaoral <x456487@fi.muni.cz>
2022-11-12 14:06:19 +0100

    instrumentation: add documentation comments to AbstractProfilerProcessor

--- src/main/java/jcprofiler/instrumentation/processors/AbstractProfilerProcessor.java
+++ src/main/java/jcprofiler/instrumentation/processors/AbstractProfilerProcessor.java
@@ -8,20 +8,44 @@ import spoon.reflect.CtModel;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtType;
 
+/**
+ * Base class for modification of the source code.
+ *
+ * @param <T> processed AST node type and an instance of {@link CtElement}
+ */
 public abstract class AbstractProfilerProcessor<T extends CtElement> extends AbstractProcessor<T> {
+    /**
+     * Commandline arguments
+     */
     protected final Args args;
 
+    /**
+     * Instance of the PM class
+     */
     protected CtType<?> PM;
+
+    /**
+     * Instance of the PMC class
+     */
     protected CtType<?> PMC;
 
+    /**
+     * Constructs the {@link AbstractProfilerProcessor} class.
+     *
+     * @param args object with commandline arguments
+     */
     protected AbstractProfilerProcessor(final Args args) {
         this.args = args;
     }
 
+    /**
+     * Initializes the given {@link spoon.processing.Processor} instance.
+     */
     @Override
     public void init() {
         super.init();
 
+        // initialise PM and PMC fields
         final CtModel model = getFactory().getModel();
         PM = JCProfilerUtil.getToplevelType(model, "PM");
         PMC = JCProfilerUtil.getToplevelType(model, "PMC");
