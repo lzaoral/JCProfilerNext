7ebe666cc889deacfb5280ec07386ed4f5e41add
Lukáš Zaoral <x456487@fi.muni.cz>
2022-11-15 09:35:53 +0100

    instrumentation: add documentation comments to entry point processors

--- src/main/java/jcprofiler/instrumentation/processors/ModifyCustomEntryPointProcessor.java
+++ src/main/java/jcprofiler/instrumentation/processors/ModifyCustomEntryPointProcessor.java
@@ -11,21 +11,48 @@ import spoon.reflect.declaration.CtClass;
 import java.io.IOException;
 import java.nio.file.Files;
 
+/**
+ * Class for modification of entry point classes in custom mode
+ */
 public class ModifyCustomEntryPointProcessor extends AbstractModifyEntryPointProcessor {
+    /**
+     * Constructs the {@link ModifyCustomEntryPointProcessor} class.
+     *
+     * @param args object with commandline arguments
+     */
     public ModifyCustomEntryPointProcessor(final Args args) {
         super(args);
     }
 
+    /**
+     * Decides whether the input {@link CtClass} should be processed.
+     *
+     * @param  cls the candidate class
+     * @return     true if yes, otherwise false
+     */
     @Override
     public boolean isToBeProcessed(final CtClass<?> cls) {
         return args.customHandler != null && super.isToBeProcessed(cls);
     }
 
+    /**
+     * Inserts an {@code INS_PERF_CUSTOM} instruction and its handler
+     * into a given {@link CtClass} instance.
+     *
+     * @param cls class to be processed
+     */
     @Override
     public void process(final CtClass<?> cls) {
         process(cls, "INS_PERF_CUSTOM");
     }
 
+    /**
+     * Creates a body of the {@code INS_PERF_CUSTOM} instruction handler.
+     *
+     * @param  apdu process method argument instance
+     * @return      a {@link CtBlock} instance with the {@code INS_PERF_CUSTOM}
+     *              instruction handler body
+     */
     @Override
     protected CtBlock<Void> createInsHandlerBody(final CtVariableRead<APDU> apdu) {
         try {
--- src/main/java/jcprofiler/instrumentation/processors/ModifyTimeEntryPointProcessor.java
+++ src/main/java/jcprofiler/instrumentation/processors/ModifyTimeEntryPointProcessor.java
@@ -9,16 +9,37 @@ import spoon.reflect.code.CtInvocation;
 import spoon.reflect.code.CtVariableRead;
 import spoon.reflect.declaration.CtClass;
 
+/**
+ * Class for modification of entry point classes in time mode
+ */
 public class ModifyTimeEntryPointProcessor extends AbstractModifyEntryPointProcessor {
+    /**
+     * Constructs the {@link ModifyTimeEntryPointProcessor} class.
+     *
+     * @param args object with commandline arguments
+     */
     public ModifyTimeEntryPointProcessor(final Args args) {
         super(args);
     }
 
+    /**
+     * Inserts an {@code INS_PERF_SETSTOP} instruction and its handler
+     * into a given {@link CtClass} instance.
+     *
+     * @param cls class to be processed
+     */
     @Override
     public void process(final CtClass<?> cls) {
         process(cls, "INS_PERF_SETSTOP");
     }
 
+    /**
+     * Creates a body of the {@code INS_PERF_SETSTOP} instruction handler.
+     *
+     * @param  apdu process method argument instance
+     * @return      a {@link CtBlock} instance with the {@code INS_PERF_SETSTOP}
+     *              instruction handler body
+     */
     @Override
     protected CtBlock<Void> createInsHandlerBody(final CtVariableRead<APDU> apdu) {
         // PM.set(${param})
