cb9905a61c861d942b51957382a3a37ae1f27db4
Lukáš Zaoral <x456487@fi.muni.cz>
2022-11-16 22:00:38 +0100

    visualisation: add documentation comments to AbstractInsertMeasurementsProcessor

--- src/main/java/jcprofiler/visualisation/processors/AbstractInsertMeasurementsProcessor.java
+++ src/main/java/jcprofiler/visualisation/processors/AbstractInsertMeasurementsProcessor.java
@@ -13,17 +13,39 @@ import spoon.reflect.reference.CtExecutableReference;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * General class for annotation of profiled sources
+ */
 public abstract class AbstractInsertMeasurementsProcessor extends AbstractProcessor<CtInvocation<Void>> {
+    /**
+     * Commandline arguments
+     */
     protected final Args args;
+    /**
+     * Map between traps and measurements
+     */
     protected final Map<String, List<Long>> measurements;
 
     private static final Logger log = LoggerFactory.getLogger(AbstractInsertMeasurementsProcessor.class);
 
+    /**
+     * Constructs the {@link AbstractInsertMeasurementsProcessor} class.
+     *
+     * @param args         object with commandline arguments
+     * @param measurements map between traps and measurements
+     */
     protected AbstractInsertMeasurementsProcessor(final Args args, final Map<String, List<Long>> measurements) {
         this.args = args;
         this.measurements = measurements;
     }
 
+    /**
+     * Decides whether the input {@link CtInvocation} corresponds to
+     * a {@code PM#check(short)} call.
+     *
+     * @param  statement the candidate invocation
+     * @return           true if yes, otherwise false
+     */
     @Override
     public boolean isToBeProcessed(final CtInvocation<Void> statement) {
         final CtExecutableReference<?> executable = statement.getExecutable();
@@ -33,8 +55,21 @@ public abstract class AbstractInsertMeasurementsProcessor extends AbstractProces
                executable.isStatic();
     }
 
+    /**
+     * Returns a commentary contents to replace the {@code PM#check(short)} call
+     * for given performance trap.
+     *
+     * @param  fieldName name of the performance trap field
+     * @return           comment annotation contents
+     */
     protected abstract String getCommentString(final String fieldName);
 
+    /**
+     * Replaces the {@code PM#check(short)} calls with
+     * a commentary with measurement statistics.
+     *
+     * @param invocation invocation to be processed
+     */
     @Override
     public void process(final CtInvocation<Void> invocation) {
         final CtFieldRead<?> trapFieldRead = (CtFieldRead<?>) invocation.getArguments().get(0);
