f88088f89b4302c7dfa6a6bd00d8bdc7b2d9efb0
Lukáš Zaoral <x456487@fi.muni.cz>
2022-11-16 22:01:23 +0100

    visualisation: add documentation comments to code annotation processors

--- src/main/java/jcprofiler/visualisation/processors/InsertMemoryMeasurementsProcessor.java
+++ src/main/java/jcprofiler/visualisation/processors/InsertMemoryMeasurementsProcessor.java
@@ -5,11 +5,27 @@ import jcprofiler.args.Args;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Class for annotation of profiled sources in memory mode
+ */
 public class InsertMemoryMeasurementsProcessor extends AbstractInsertMeasurementsProcessor {
+    /**
+     * Constructs the {@link InsertMemoryMeasurementsProcessor} class.
+     *
+     * @param args         object with commandline arguments
+     * @param measurements map between traps and measurements
+     */
     public InsertMemoryMeasurementsProcessor(final Args args, final Map<String, List<Long>> measurements) {
         super(args, measurements);
     }
 
+    /**
+     * Returns a commentary contents to replace the {@code PM#check(short)} call
+     * for given performance trap.
+     *
+     * @param  fieldName name of the performance trap field
+     * @return           comment annotation contents
+     */
     @Override
     protected String getCommentString(String fieldName) {
         final List<Long> values = measurements.get(fieldName);
--- src/main/java/jcprofiler/visualisation/processors/InsertTimeMeasurementsProcessor.java
+++ src/main/java/jcprofiler/visualisation/processors/InsertTimeMeasurementsProcessor.java
@@ -8,15 +8,32 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * Class for annotation of profiled sources in time mode
+ */
 public class InsertTimeMeasurementsProcessor extends AbstractInsertMeasurementsProcessor {
     private final Map<String, DescriptiveStatistics> statisticsMap;
 
+    /**
+     * Constructs the {@link InsertTimeMeasurementsProcessor} class.
+     *
+     * @param args          object with commandline arguments
+     * @param measurements  map between traps and measurements
+     * @param statisticsMap map between traps and measurement statistics
+     */
     public InsertTimeMeasurementsProcessor(final Args args, final Map<String, List<Long>> measurements,
                                            final Map<String, DescriptiveStatistics> statisticsMap) {
         super(args, measurements);
         this.statisticsMap = statisticsMap;
     }
 
+    /**
+     * Returns a commentary contents to replace the {@code PM#check(short)} call
+     * for given performance trap.
+     *
+     * @param  fieldName name of the performance trap field
+     * @return           comment annotation contents
+     */
     @Override
     protected String getCommentString(String fieldName) {
         final List<Long> values = measurements.get(fieldName);
