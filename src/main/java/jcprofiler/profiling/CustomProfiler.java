c551b3c73ba5207d1a2fce352006ee2aef9f8b3c
Lukáš Zaoral <x456487@fi.muni.cz>
2022-11-16 15:03:00 +0100

    profiling: add documentation to CustomProfiler

--- src/main/java/jcprofiler/profiling/CustomProfiler.java
+++ src/main/java/jcprofiler/profiling/CustomProfiler.java
@@ -12,19 +12,38 @@ import spoon.reflect.declaration.CtConstructor;
 
 import java.io.IOException;
 
-// custom profiler will only generate inputs!
+/**
+ * This class represents the specifics of profiling in custom mode.
+ */
 public class CustomProfiler extends AbstractProfiler {
+    /**
+     * Constructs the {@link CustomProfiler} class.
+     *
+     * @param args        object with commandline arguments
+     * @param cardManager applet connection instance
+     * @param model       Spoon model
+     */
     public CustomProfiler(final Args args, final CardManager cardManager, final CtModel model) {
         super(args, cardManager, JCProfilerUtil.getProfiledExecutable(model, args.entryPoint, args.executable),
               /* customInsField */ null);
     }
 
+    /**
+     * Generates the list of inputs for custom profiling.
+     */
     @Override
     protected void profileImpl() {
         if (!(profiledExecutable instanceof CtConstructor))
             generateInputs(args.repeatCount);
     }
 
+    /**
+     * Stores the measurements template using given {@link CSVPrinter} instance.
+     *
+     * @param  printer instance of the CSV printer
+     *
+     * @throws IOException if the printing fails
+     */
     @Override
     protected void saveMeasurements(final CSVPrinter printer) throws IOException {
         printer.printComment("trapName,measurement1,measurement2,...");
